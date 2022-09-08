b8f4374d8e2704dfcf03beb88d8a6942043c9fa1
procfs <woosun.song@dream-academy.io>
2022-09-08 18:01:19 +0900

    implement quiz

--- src/Quiz.sol
+++ src/Quiz.sol
@@ -10,8 +10,10 @@ contract Quiz{
       uint max_bet;
    }
     
-    mapping(address => uint256)[] public bets;
+    Quiz_item[] quiz_list;
+    mapping(uint256 => mapping(address => uint256)) public bets;
     uint public vault_balance;
+    address owner;
 
     constructor () {
         Quiz_item memory q;
@@ -20,28 +22,89 @@ contract Quiz{
         q.answer = "2";
         q.min_bet = 1 ether;
         q.max_bet = 2 ether;
+        owner = msg.sender;
+        bets[0][address(0xdeadbeef)] = 0;
         addQuiz(q);
     }
 
     function addQuiz(Quiz_item memory q) public {
+        if (msg.sender != owner) {
+            revert("");
+        }
+        for (uint i = 0; i < quiz_list.length; i++) {
+            if (quiz_list[i].id == q.id) {
+                revert("");
+            }
+        }
+        quiz_list.push(q);
     }
 
     function getAnswer(uint quizId) public view returns (string memory){
+        for (uint i = 0; i < quiz_list.length; i++) {
+            if (quiz_list[i].id == quizId) {
+                return quiz_list[i].answer;
+            }
+        }
+        return "";
     }
 
     function getQuiz(uint quizId) public view returns (Quiz_item memory) {
+        for (uint i = 0; i < quiz_list.length; i++) {
+            if (quiz_list[i].id == quizId) {
+                Quiz_item memory q;
+                q = quiz_list[i];
+                q.answer = "";
+                return q;
+            }
+        }
     }
 
     function getQuizNum() public view returns (uint){
+        return quiz_list.length;
     }
     
     function betToPlay(uint quizId) public payable {
+        for (uint i = 0; i < quiz_list.length; i++) {
+            if (quiz_list[i].id == quizId) {
+                if (msg.value < quiz_list[i].min_bet || msg.value > quiz_list[i].max_bet) {
+                    revert("");
+                }
+                else {
+                    address sender = msg.sender;
+                    bets[0][sender] += msg.value;
+                    return;
+                }
+            }
+        }
+        revert("");
+        
     }
 
     function solveQuiz(uint quizId, string memory ans) public returns (bool) {
+        for (uint i = 0; i < quiz_list.length; i++) {
+            if (quiz_list[i].id == quizId) {
+                if (keccak256(bytes(quiz_list[i].answer)) == keccak256(bytes(ans))) {
+                    return true;
+                }
+                else {
+                    vault_balance += bets[0][msg.sender];
+                    bets[0][msg.sender] = 0;
+                    return false;
+                }
+            }
+        }
+        revert("");
     }
 
     function claim() public {
+        address payable recipient = payable(msg.sender);
+        vault_balance -= 2 * bets[0][recipient];
+        recipient.transfer(2 * bets[0][recipient]);
+        bets[0][recipient]= 0;
+    }
+
+    receive() external payable {
+        vault_balance += msg.value;
     }
 
 }
